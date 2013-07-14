e13a6f400312a5aa7451953201c135ddc8ea6226
Andres Dominguez <andres.dominguez.p@gmail.com>
2013-07-14 19:41:56 -0400

    add test for left having more than left

--- test/index-spec.js
+++ test/index-spec.js
@@ -13,6 +13,19 @@ describe('give-me-diff', function() {
         'name: Andres|andres\n');
   });
 
+  it('should compare when left has more properties', function() {
+    var left = {
+      name: 'Andres',
+      tel: 123
+    };
+    var right = {name: 'andres'};
+
+    expect(diff.compare(left, right)).toEqual('Diff:\n\n' +
+        'name: Andres|andres\n' +
+        'tel: 123|undefined\n');
+
+  });
+
   it('should return undefined when object are equal', function() {
     expect(diff.compare({name: 'foo'}, {name: 'foo'})).toBeUndefined();
   });
